69fb5757480bcf0b7c8d7678cb000f5fd7b3bbfe
Silvia Fabro <ssilviaffabro@gmail.com>
2026-03-26 09:49:04 -0300

    Display total sum of array elements
    Added console log to display total sum of array elements.

--- Silvia/exe25.js
+++ Silvia/exe25.js
@@ -3,31 +3,16 @@
 // Soma dos elementos de um array
 // ------------------------------
 
-// Declaramos a função com um parâmetro chamado "numeros"
 function somarArray(numeros) {
-
-  // Variável que vai acumulando a soma
   let soma = 0;
-
-  // Percorremos cada elemento do array com o for
   for (let i = 0; i < numeros.length; i++) {
-
-    // A cada repetição, somamos o elemento atual
     soma = soma + numeros[i];
 
   }
-
-  // Retornamos o total
   return soma;
-
 }
 
-// Criamos um array de números
 let meuArray = [10, 20, 30, 40];
-
-// Chamamos a função passando o array
 let resultado = somarArray(meuArray);
-
-// Exibimos o resultado
 console.log("Array: " + meuArray);
 console.log("Soma total: " + resultado);
