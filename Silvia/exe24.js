62c2523fbf1bacc16d36fca079cb8b046c52a890
Silvia Fabro <ssilviaffabro@gmail.com>
2026-03-26 09:47:17 -0300

    Enhance Celsius to Fahrenheit converter output
    Added console log to display conversion result.

--- Silvia/exe24.js
+++ Silvia/exe24.js
@@ -3,19 +3,10 @@
 // Conversor de Celsius para Fahrenheit
 // ----------------------------------------
  
-// Declaramos a função com um parâmetro chamado "celsius"
 function converter(celsius) {
- 
-  // Fazemos o cálculo da conversão
   let resultado = (celsius * 9 / 5) + 32;
- 
-  // Retornamos o resultado
-  return resultado;
- 
+  return resultado; 
 }
- 
-// Chamamos a função passando um valor
+
 let resposta = converter(100);
- 
-// Le no terminal o resultado : 
 console.log("100°C em Fahrenheit é: " + resposta + "°F");
