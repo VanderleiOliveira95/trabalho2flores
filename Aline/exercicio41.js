8f2accd728da6f816a1cd10d21582d95590518a1
Aline rodrigues <allinerodriguesl2000@gmail.com>
2026-03-30 17:43:05 -0300

    Convert 'soma' function to arrow function
    Converted the traditional function 'soma' to an arrow function.

--- Aline/exercicio41.js
+++ Aline/exercicio41.js
@@ -1,1 +1,18 @@
+/******************************************************************************
+Curso: Tecnologia em Análise e Desenvolvimento de Sistemas
+Disciplina: Programação Front-End
+Professor: José Carlos Flores
+Turma: ADS3B
+Componentes:
+ 25246894-2 - Lucas Alves Bonnet
+ 25153556-2 - Aline Rodrigues Iorkievicz
+ 25036114-2 - Vanderlei Oliveira Junior
+ 25181953-2 - Gilberto Espanga Junior
+ 25216347-2 - Kauany Paula da Silva
+ 25355474-2 - Silvia de Oliveira Fabro
+ 25178018-2 - Yuri Kauan de Paula
+Data: 05 de Março de 2026
+Descritivo:41. Converta a função `function soma(a,b) { return a+b }` para Arrow Function.
 
+*******************************************************************************/
+//41. Converta a função `function soma(a,b) { return a+b }` para Arrow Function.
