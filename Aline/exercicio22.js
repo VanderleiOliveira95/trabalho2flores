61c946925560e81831986910da5121a9dfa5481c
Aline rodrigues <allinerodriguesl2000@gmail.com>
2026-03-30 12:17:52 -0300

    Add function description for areaRetangulo

--- Aline/exercicio22.js
+++ Aline/exercicio22.js
@@ -1,1 +1,17 @@
+/******************************************************************************
+Curso: Tecnologia em Análise e Desenvolvimento de Sistemas
+Disciplina: Programação Front-End
+Professor: José Carlos Flores
+Turma: ADS3B
+Componentes:
+ 25246894-2 - Lucas Alves Bonnet
+ 25153556-2 - Aline Rodrigues Iorkievicz
+ 25036114-2 - Vanderlei Oliveira Junior
+ 25181953-2 - Gilberto Espanga Junior
+ 25216347-2 - Kauany Paula da Silva
+ 25355474-2 - Silvia de Oliveira Fabro
+ 25178018-2 - Yuri Kauan de Paula
+Data: 05 de Março de 2026
+Descritivo: 22. Crie uma função `areaRetangulo(base, altura)` que retorne a área.*******************************************************************************/
+//22. Crie uma função `areaRetangulo(base, altura)` que retorne a área.21. Crie uma função `soma(a, b)` que retorne a soma de dois números.
 
